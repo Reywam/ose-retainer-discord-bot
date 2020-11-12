2c206c314bd479e52e9c55f81510ed4be7b6adef
wardenrok <reywam@mail.ru>
2020-11-12 19:16:09 +0300

    Implement part of elf strategy

--- src/main/java/character/classes/strategies/ElfEquipmentStrategy.java
+++ src/main/java/character/classes/strategies/ElfEquipmentStrategy.java
@@ -1,17 +1,23 @@
 package character.classes.strategies;
 
+import character.classes.EquipmentDataWorker;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Arrays;
 
 @Data
 @NoArgsConstructor
 public class ElfEquipmentStrategy implements EquipmentStrategy {
     @Override
-    public ObjectNode getEquipment(JsonNode equipment) {
+    public JsonNode getEquipment(JsonNode equipment) {
         // TODO Actually like a fighter strategy
-        System.out.println("Elf equipment strategy");
-        return null;
+        ArrayNode meleeWeapons = EquipmentDataWorker.getNWeaponsOfType(1, "melee", equipment);
+        ArrayNode missileWeapons = EquipmentDataWorker.getNWeaponsOfType(1, "missile", equipment);
+        ArrayNode weapons = meleeWeapons.addAll(missileWeapons);
+        JsonNode armor = EquipmentDataWorker.getArmorOfSomeType(Arrays.asList("heavy", "medium", "light"), equipment);
+        return EquipmentDataWorker.aggregateEquipment(weapons, armor);
     }
 }
